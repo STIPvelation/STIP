a92cd377dae59742d468546fa45e48105459a359
Lee dong hyeon <ymhpro@naver.com>
2024-12-16 11:32:57 +0900

    24-12-16

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js
+// Your API Key: 90809c4e6dde58e47c6544bb
+// Example Request: https://v6.exchangerate-api.com/v6/90809c4e6dde58e47c6544bb/latest/USD
+export const EXCHANGE_API_KEY = '90809c4e6dde58e47c6544bb';
